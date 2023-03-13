081e3495164ecef96f8450fc065d2368ebc4f9a6
tzbodula <tzbodula@gmail.com>
2023-03-13 09:37:50 -0400

    Portfolio Section

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-     
+        <Portfolio/>
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
